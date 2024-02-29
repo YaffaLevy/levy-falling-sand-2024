c02f5835d715a363b47b294984a10df970940903
Yaffa Levy <yaffajlevy@gmail.com>
2024-02-28 20:23:34 -0500

    added sand class and test

---
+++ src/main/java/levy/fallingsand/Sand.java
@@ -0,0 +1,37 @@
+package levy.fallingsand;
+
+public class Sand {
+
+    private int[][] field = new int[3][3];
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                builder.append((field[y][x]));
+            }
+            builder.append("\n");
+
+        }
+        return builder.toString();
+
+    }
+
+    public int gett(int x, int y) {
+        return field[y][x];
+
+    }
+
+    public void put(int x, int y) {
+        field[y][x] = 1;
+    }
+
+    public void fall(){
+        // moves all sand down one square
+    }
+
+
+}
+
+
+
