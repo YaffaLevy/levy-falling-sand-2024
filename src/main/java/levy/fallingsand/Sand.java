53affdf5a336d803b784019480e9178c2d546a1d
Yaffa Levy <yaffajlevy@gmail.com>
2024-03-05 16:53:02 -0500

    step 1 of sand homework

--- src/main/java/levy/fallingsand/Sand.java
+++ src/main/java/levy/fallingsand/Sand.java
@@ -1,37 +1,41 @@
 package levy.fallingsand;
 
-public class Sand {
+    public class Sand {
 
-    private int[][] field = new int[3][3];
+        private int[][] field = new int[3][3];
 
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
-                builder.append((field[y][x]));
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    builder.append(field[y][x]);
+                }
+                builder.append("\n");
             }
-            builder.append("\n");
-
+            return builder.toString();
         }
-        return builder.toString();
+
+        public int get(int x, int y) {
+            return field[y][x];
+        }
+
+        public void put(int x, int y) {
+            field[y][x] = 1;
+        }
+
+        public void fall() {
+            for (int y = 2; y >= 0; y--) {
+                for (int x = 0; x < 3; x++) {
+                    if (field[y][x] == 1 && y < 2 && field[y + 1][x] == 0) {
+                        field[y + 1][x] = 1;
+                        field[y][x] = 0;
+                    }
+                }
+            }
+        }
 
     }
 
-    public int gett(int x, int y) {
-        return field[y][x];
-
-    }
-
-    public void put(int x, int y) {
-        field[y][x] = 1;
-    }
-
-    public void fall(){
-        // moves all sand down one square
-    }
-
-
-}
 
 
 
