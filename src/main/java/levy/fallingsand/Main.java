eda163fe9734960f4debc968de4102c1edc18024
Yaffa Levy <yaffajlevy@gmail.com>
2024-03-19 20:47:33 -0400

    again

--- src/main/java/levy/fallingsand/Main.java
+++ src/main/java/levy/fallingsand/Main.java
@@ -1,29 +1,17 @@
-
 package levy.fallingsand;
-
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-
-        Scanner scanner = new Scanner(System.in);
         Sand sand = new Sand(50, 10);
         sand.randomSand(50);
 
         SandComponent sandComponent = new SandComponent(sand);
         sandComponent.initializeGui();
-
-        while (true) {
-            System.out.println(sand);
-            System.out.println("Press Enter to make sand fall");
-            scanner.nextLine();
-            sand.fall();
-        }
     }
 
 }
+
 
 
 
